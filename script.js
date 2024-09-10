57e02bdf3688d214aa04fc2e6e831ec2d806efec
Moses Odhiambo <125870765+Mosesodhiambooo@users.noreply.github.com>
2024-09-11 00:49:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-const swiper = new Swiper('.slider-wrapper', {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 30,
-
-  // Pagination bullets
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    0: {
-      slidesPerView: 1
-    },
-    768: {
-      slidesPerView: 2
-    },
-    1024: {
-      slidesPerView: 3
-    }
-  }
-});
 
 // search-box open close js code
 let navbar = document.querySelector(".navbar");
@@ -56,17 +25,54 @@ navLinks.style.left = "-100%";
 }
 
 
-// sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function() {
- navLinks.classList.toggle("show1");
-}
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
- navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function() {
- navLinks.classList.toggle("show3");
+var swiper = new Swiper('.swiper', {
+      slidesPerView: 1, /* Show 1 slide at a time on mobile */
+      spaceBetween: 10,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        // Show 3 slides at a time on larger devices
+        768: {
+          slidesPerView: 3,
+        }
+      }
+    });
+
+
+    let currentPage = 0;
+const itemsPerPage = 1; // Only one image per page on mobile view
+const galleryRow = document.querySelector('.gallery-row');
+const totalItems = document.querySelectorAll('.gallery-item').length;
+
+function updateGallery() {
+  const offset = currentPage * itemsPerPage * -100;
+  galleryRow.style.transform = `translateX(${offset}%)`;
 }
 
+function nextPage() {
+  if (currentPage < totalItems - 1) {
+    currentPage++;
+    updateGallery();
+  }
+}
+
+function prevPage() {
+  if (currentPage > 0) {
+    currentPage--;
+    updateGallery();
+  }
+}
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768) {
+    galleryRow.style.transform = 'none'; // Reset for larger screens
+  } else {
+    updateGallery();
+  }
+});
